621f25d4b7d2d077bca8230d8cbcc8a58e31bed8
IvanPavlovets <ivan150287@gmail.com>
2025-10-15 19:29:22 +0700

    init

---
+++ src/main/java/com/example/user_exporter_service/UserExporterServiceApp.java
@@ -0,0 +1,13 @@
+package com.example.user_exporter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserExporterServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserExporterServiceApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_exporter_service/UserExporterServiceAppTests.java
@@ -0,0 +1,13 @@
+package com.example.user_exporter_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserExporterServiceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
